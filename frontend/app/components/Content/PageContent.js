a86a954ef9696f71a85153ed7424da6256a8ec0e
Brian Cappello <briancappello@gmail.com>
2016-11-12 06:55:48 -0500

    add content class to PageContent component

--- frontend/app/components/Content/PageContent.js
+++ frontend/app/components/Content/PageContent.js
@@ -8,7 +8,7 @@ export default class PageContent extends Component {
     render() {
         return (
             <div className="container">
-                <div className={`${this.props.className}`}>
+                <div className={`${this.props.className} content`}>
                     {this.props.children}
                 </div>
             </div>
