b81798a215535818ad9716ea4868f8cfb12e47ba
Brian Cappello <briancappello@gmail.com>
2016-11-12 06:52:05 -0500

    update manage.py to run in debug by default, and warn when in prod

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask.helpers import get_debug_flag
 
-from backend.app import create_app
+from backend.app import create_app as _create_app
 from backend.config import (
     DevConfig,
     ProdConfig,
@@ -9,10 +9,15 @@ from backend.config import (
     STATIC_URL_PATH,
 )
 
-# default to ProdConfig unless FLASK_DEBUG env var is explicitly set to true
-app = create_app(
-    DevConfig if get_debug_flag(False) else ProdConfig,
-    template_folder=TEMPLATE_FOLDER,
-    static_folder=STATIC_FOLDER,
-    static_url_path=STATIC_URL_PATH
-)
+
+def create_app():
+    return _create_app(
+        # default to ProdConfig unless FLASK_DEBUG env var is explicitly set to true
+        DevConfig if get_debug_flag() else ProdConfig,
+        template_folder=TEMPLATE_FOLDER,
+        static_folder=STATIC_FOLDER,
+        static_url_path=STATIC_URL_PATH
+    )
+
+
+app = create_app()
--- manage.py
+++ manage.py
@@ -1,25 +1,57 @@
+#!/usr/bin/env python
 """
 This script is equivalent to the following:
-FLASK_APP=app.py flask
+FLASK_APP=app.py FLASK_DEBUG=true flask
+
+(By default, always runs with FLASK_DEBUG=true, unless --env=prod is passed or
+FLASK_DEBUG is set to false, no or 0)
+
+USAGE:
+python manage.py COMMAND [OPTIONS] [ARGS] [--env=prod]
 """
 import os
+import sys
+import click
+import time
 from flask.cli import FlaskGroup
+from flask.helpers import get_debug_flag
 
-from app import app
+from app import create_app
 
-cli = FlaskGroup(create_app=lambda _: app, help="""\
-A utility script for the Flask API application.
 
-Dev usage:
+def production_warning(args):
+    if len(args):
+        # allow some time to cancel commands
+        for i in [5, 4, 3, 2, 1]:
+            click.echo('!!! PRODUCTION !!!: Starting "%s" in %d seconds' % (' '.join(args), i))
+            time.sleep(1)
 
-  %(prefix)sFLASK_DEBUG=1 python manage.py
 
-Prod usage:
+def get_args():
+    """Returns a tuple of (args, is_debug)"""
+    args = sys.argv[1:]
+    # default to DEBUG unless production explicitly set
+    if not get_debug_flag(True) or '--env=prod' in args:
+        prod_args = [arg for arg in args if arg != '--env=prod']
+        production_warning(prod_args)
+        return prod_args, False
+    return args, True
 
-  %(prefix)spython manage.py
-""" % {
-    'prefix': os.name == 'posix' and '$ ' or '',
-})
 
 if __name__ == '__main__':
-    cli.main()
+    args, is_debug = get_args()
+
+    if is_debug:
+        os.environ['FLASK_DEBUG'] = 'true'
+
+    cli = FlaskGroup(create_app=lambda _: create_app(), help="""\
+A utility script for the Flask API application.
+
+Usage:
+
+  %(prefix)spython manage.py COMMAND [OPTIONS] [ARGS] [--env=prod]
+    """ % {
+        'prefix': '$ ' if os.name == 'posix' else '',
+    })
+
+    cli.main(args=args)
