cf5bb8e70f1ff733bf9659fc6bf536515bdff5d3
Brian Cappello <briancappello@gmail.com>
2016-11-15 21:13:49 -0500

    add join_table function

--- backend/database.py
+++ backend/database.py
@@ -14,22 +14,6 @@ Integer = db.Integer            # type: sqlalchemy.types.Integer
 DateTime = db.DateTime          # type: sqlalchemy.types.DateTime
 relationship = db.relationship  # type: RelationshipProperty
 
-# RELATIONSHIP DOCS
-# http://flask-sqlalchemy.pocoo.org/2.1/models/#one-to-many-relationships
-# http://flask-sqlalchemy.pocoo.org/2.1/models/#many-to-many-relationships
-# http://docs.sqlalchemy.org/en/rel_1_0/orm/basic_relationships.html#relationship-patterns
-# http://docs.sqlalchemy.org/en/rel_1_0/orm/backref.html#relationships-backref
-
-
-def foreign_key(table_name, nullable=False, **kwargs):
-    """Adds a foreign key column.
-
-    Usage: ::
-        category_id = foreign_key('category')
-        category = relationship('Category', back_populates='categories')
-    """
-    return Column(Integer, db.ForeignKey(table_name + '.id'), nullable=nullable, **kwargs)
-
 
 class Model(db.Model):
     """Base table class with primary key and convenience methods."""
@@ -84,3 +68,40 @@ class Model(db.Model):
         properties = ['{!s}={!r}'.format(prop, getattr(self, prop))
                       for prop in self._repr_props_() if hasattr(self, prop)]
         return '<{} id={} {}>'.format(self.__class__.__name__, self.id, ' '.join(properties))
+
+
+# RELATIONSHIP DOCS
+# http://flask-sqlalchemy.pocoo.org/2.1/models/#one-to-many-relationships
+# http://flask-sqlalchemy.pocoo.org/2.1/models/#many-to-many-relationships
+# http://docs.sqlalchemy.org/en/rel_1_0/orm/basic_relationships.html#relationship-patterns
+# http://docs.sqlalchemy.org/en/rel_1_0/orm/backref.html#relationships-backref
+
+
+def foreign_key(table_name, nullable=False, **kwargs):
+    """Adds a foreign key column.
+
+    Usage: ::
+        category_id = foreign_key('category')
+        category = relationship('Category', back_populates='categories')
+    """
+    return Column(Integer, db.ForeignKey(table_name + '.id'), nullable=nullable, **kwargs)
+
+
+def join_table(model_name1, model_name2, *extra_columns):
+    """Creates a join table.
+
+    Usage: ::
+        symbol_industry = join_table('Symbol', 'Industry')
+        class Symbol(Model):
+            industries = relationship('Industry', secondary=symbol_industry, back_populates='symbols')
+        class Industry(Model)
+            symbols = relationship('Symbol', secondary=symbol_industry, back_populates='industries')
+    """
+    table1 = model_name1.lower()
+    table2 = model_name2.lower()
+    return Table(
+        '{}_{}'.format(table1, table2),
+        Column('{}_id'.format(table1), Integer, db.ForeignKey('{}.id'.format(table1))),
+        Column('{}_id'.format(table2), Integer, db.ForeignKey('{}.id'.format(table2))),
+        *extra_columns
+    )
