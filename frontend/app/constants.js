4038c6366f7f8970b84bc2c45c60be8ce7b64e60
Brian Cappello <briancappello@gmail.com>
2016-11-18 07:55:49 -0500

    add basic logging setup

---
+++ frontend/app/constants.js
@@ -0,0 +1,10 @@
+//-------------------------
+// CONSTANTS
+//-------------------------
+
+// debug levels
+export const CRITICAL = 'CRITICAL'
+export const ERROR = 'ERROR'
+export const WARNING = 'WARNING'
+export const INFO = 'INFO'
+export const DEBUG = 'DEBUG'
