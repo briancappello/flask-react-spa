79ac6132a2d3f76cb02ba13d408069ab1f9ddb49
Brian Cappello <briancappello@gmail.com>
2016-12-17 11:11:03 -0500

    specify naming conventions for sqlalchemy foreign keys / indexes / constraints

--- backend/extensions.py
+++ backend/extensions.py
@@ -1,11 +1,18 @@
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
+from sqlalchemy import MetaData
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_login import LoginManager
 
 bcrypt = Bcrypt()
-db = SQLAlchemy()
+db = SQLAlchemy(metadata=MetaData(naming_convention={
+    'ix': 'ix_%(column_0_label)s',
+    'uq': 'uq_%(table_name)s_%(column_0_name)s',
+    'ck': 'ck_%(table_name)s_%(constraint_name)s',
+    'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s',
+    'pk': 'pk_%(table_name)s',
+}))
 migrate = Migrate(db=db)
 jwt = JWTManager()
 login_manager = LoginManager()
