78de517fed47e13c4abe1f22072735583d45557b
Brian Cappello <briancappello@gmail.com>
2016-12-17 11:12:07 -0500

    specify render_as_batch to alembic for migrations compatibility between sqlite and mysql/postgresql

--- backend/extensions.py
+++ backend/extensions.py
@@ -13,6 +13,6 @@ db = SQLAlchemy(metadata=MetaData(naming_convention={
     'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s',
     'pk': 'pk_%(table_name)s',
 }))
-migrate = Migrate(db=db)
+migrate = Migrate(db=db, render_as_batch=True)
 jwt = JWTManager()
 login_manager = LoginManager()
