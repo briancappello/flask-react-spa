cd5c0b320ba4de46f615cfbf4aa029a04a72d657
Brian Cappello <briancappello@gmail.com>
2017-08-31 00:03:20 -0400

    update prod webpack config

--- frontend/internals/webpack/webpack.prod.config.js
+++ frontend/internals/webpack/webpack.prod.config.js
@@ -1,20 +1,51 @@
 const path = require('path')
 const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = require('./webpack.base.config.js')({
   entry: {
     app: path.join(process.cwd(), 'frontend', 'app', 'index.js'),
   },
+
+  // Utilize long-term caching by adding content hashes (not compilation hashes) to compiled assets
+  output: {
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].chunk.js',
+  },
+
   plugins: [
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      children: true,
+      minChunks: 2,
+      async: true,
+    }),
+
+    // Minify and optimize the index.html
+    new HtmlWebpackPlugin({
+      template: 'frontend/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: false,
+        minifyCSS: true,
+        minifyURLs: true,
+      },
+      inject: true,
+    }),
+
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify('production'),
       },
     }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-      debug: false,
-    }),
+
     new webpack.optimize.UglifyJsPlugin(),
   ],
 })
