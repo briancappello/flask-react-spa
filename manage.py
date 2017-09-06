56a702d214e950df1403d4cb876baa668062ea17
Brian Cappello <briancappello@gmail.com>
2017-09-05 20:04:50 -0400

    clean up the manage.py entry point to allow modifying the shared command context

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
 This script is equivalent to the following:
-FLASK_APP=app.py FLASK_DEBUG=true flask
+FLASK_APP=wsgi.py FLASK_DEBUG=true flask
 
 (By default, always runs with FLASK_DEBUG=true, unless --env=prod is passed or
 FLASK_DEBUG is set to false, no or 0)
@@ -9,49 +9,44 @@ FLASK_DEBUG is set to false, no or 0)
 USAGE:
 python manage.py COMMAND [OPTIONS] [ARGS] [--env=prod]
 """
+import argparse
 import os
 import sys
 import click
 import time
+
 from flask.cli import FlaskGroup
-from flask.helpers import get_debug_flag
-
-from create_app import create_app
+from backend.app import create_app
 
 
-def production_warning(args):
+def production_warning(env, args):
     if len(args):
+        env = 'PRODUCTION' if env == 'prod' else 'STAGING'
+        cmd = ' '.join(args)
         # allow some time to cancel commands
-        for i in [5, 4, 3, 2, 1]:
-            click.echo('!!! PRODUCTION !!!: Starting "%s" in %d seconds' % (' '.join(args), i))
+        for i in [4, 3, 2, 1]:
+            click.echo('!! %s !!: Running "%s" in %d seconds' % (env, cmd, i))
             time.sleep(1)
 
 
-def get_args():
-    """Returns a tuple of (args, is_debug)"""
-    args = sys.argv[1:]
-    # default to DEBUG unless production explicitly set
-    if not get_debug_flag(True) or '--env=prod' in args:
-        prod_args = [arg for arg in args if arg != '--env=prod']
-        production_warning(prod_args)
-        return prod_args, False
-    return args, True
+@click.group(cls=FlaskGroup, create_app=lambda _: create_app(), help="""\
+A utility script for the Flask API application.
+""")
+@click.option('--env', type=click.Choice(['dev', 'staging', 'prod']), default='dev')
+@click.option('--warn/--no-warn', default=True)
+@click.pass_context
+def cli(ctx, env, warn):
+    ctx.obj.data['env'] = env
+    if env != 'dev' and warn:
+        production_warning(env, sys.argv[2:])
 
 
 if __name__ == '__main__':
-    args, is_debug = get_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--env', default='dev')
+    args, _ = parser.parse_known_args()
 
-    if is_debug:
+    if args.env == 'dev':
         os.environ['FLASK_DEBUG'] = 'true'
 
-    cli = FlaskGroup(create_app=lambda _: create_app(), help="""\
-A utility script for the Flask API application.
-
-Usage:
-
-  %(prefix)spython manage.py COMMAND [OPTIONS] [ARGS] [--env=prod]
-    """ % {
-        'prefix': '$ ' if os.name == 'posix' else '',
-    })
-
-    cli.main(args=args)
+    cli.main(args=sys.argv[1:])
