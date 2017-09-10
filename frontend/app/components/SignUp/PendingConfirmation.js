67a8f475304a84c69a0d6df2c278f36f8649c425
Brian Cappello <briancappello@gmail.com>
2017-09-10 11:55:53 -0400

    update title tag on pending email confirmation page

--- frontend/app/components/SignUp/PendingConfirmation.js
+++ frontend/app/components/SignUp/PendingConfirmation.js
@@ -6,7 +6,7 @@ import { PageContent } from 'components/Content'
 export default () => (
   <PageContent>
     <Helmet>
-      <title>Sign Up Pending Email Confirmation</title>
+      <title>Please Confirm your Email Address</title>
     </Helmet>
     <h1>Thanks for signing up!</h1>
     <p>Please check your email to confirm your email address and login.</p>
