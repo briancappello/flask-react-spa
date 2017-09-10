354ba6f9d3b773df97b2c23da1d48a04f146aab7
Brian Cappello <briancappello@gmail.com>
2017-09-10 12:06:50 -0400

    consolidate routes into main Application component

--- frontend/app/components/Application.js
+++ frontend/app/components/Application.js
@@ -3,29 +3,60 @@ import { connect } from 'react-redux'
 import { Switch, Route } from 'react-router-dom'
 import Helmet from 'react-helmet'
 
-import { SITE_NAME, COPYRIGHT } from 'config'
+import {
+  About,
+  ForgotPassword,
+  Home,
+  Login,
+  Logout,
+  NotFound,
+  PendingConfirmation,
+  ResendConfirmation,
+  ResetPassword,
+  SignUp,
+  Styles,
+  Profile,
+  Protected,
+} from 'components'
 import { NavBar } from 'components/Nav'
-import Routes from 'routes'
+import { SITE_NAME, COPYRIGHT } from 'config'
+import ProtectedRoute from 'utils/auth'
 
 import 'main.scss'
 
-export default class Application extends Component {
-  render() {
-    return (
-      <div className="fixed-nav-top">
-        <Helmet titleTemplate={`%s - ${SITE_NAME}`}
-                defaultTitle={SITE_NAME}
-        />
-        <header>
-          <NavBar />
-        </header>
-        <main>
-          <Routes />
-        </main>
-        <footer className="center">
-           Copyright {new Date().getFullYear()} {COPYRIGHT}
-        </footer>
-      </div>
-    )
-  }
-}
+
+export default (props) => (
+  <div className="fixed-nav-top">
+    <Helmet titleTemplate={`%s - ${SITE_NAME}`}
+            defaultTitle={SITE_NAME}
+    />
+    <header>
+      <NavBar />
+    </header>
+    <main>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/styles" component={Styles} />
+        <ProtectedRoute exact path="/protected" component={Protected} />
+
+        <Route exact path="/sign-up" component={SignUp} />
+        <Route exact path="/sign-up/resend-confirmation-email" component={ResendConfirmation} />
+        <Route exact path="/sign-up/pending-confirm-email" component={PendingConfirmation} />
+
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/login/forgot-password" component={ForgotPassword} />
+        <Route exact path="/login/reset-password/:token" component={ResetPassword} />
+
+        <ProtectedRoute exact path="/profile" component={Profile} />
+        <Route exact path="/logout" component={Logout} />
+
+        {/* default 404 if no match */}
+        <Route component={NotFound} />
+      </Switch>
+    </main>
+    <footer className="center">
+       Copyright {new Date().getFullYear()} {COPYRIGHT}
+    </footer>
+  </div>
+)
