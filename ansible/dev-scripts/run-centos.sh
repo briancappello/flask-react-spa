bbadc5d18d1c9104ff1266b7ea08d8ae70ed2880
Brian Cappello <briancappello@gmail.com>
2020-01-08 17:36:00 -0500

    update to centos 7.7

--- ansible/dev-scripts/run-centos.sh
+++ ansible/dev-scripts/run-centos.sh
@@ -5,11 +5,12 @@
 vm_name="$1"
 vm_image="$vm_name.img"
 dest="/var/lib/libvirt/images/$vm_image"
-mv $vm_image $dest
-chown nobody:kvm $dest
+mv "$vm_image" "$dest"
+chown nobody:kvm "$dest"
 
+echo "running virt-install"
 virt-install --import --name "$vm_name" \
 	--ram 2048 \
 	--disk path="$dest" \
-	--os-variant rhel7 \
+	--os-variant rhel7.7 \
 	--noautoconsole
